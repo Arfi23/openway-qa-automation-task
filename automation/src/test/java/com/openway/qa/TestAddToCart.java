9bf44040d8ab2d1129eafa4c727dfb01bd5c4156
Arfi23 <arfifaizilraushani@gmail.com>
2026-04-14 14:36:23 +0700

    Tried to implement wait instead of sleep, but got an issue after login attempt

--- automation/src/test/java/com/openway/qa/TestAddToCart.java
+++ automation/src/test/java/com/openway/qa/TestAddToCart.java
@@ -2,59 +2,82 @@ package com.openway.qa;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class TestAddToCart {
     @Test
     public void testAddToCart() {
 
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
         try {
             // 1. Open website
             driver.get("https://www.periplus.com/");
             driver.manage().window().maximize();
 
-            Thread.sleep(3000);
+            // 2. Click Sign In
+            wait.until(ExpectedConditions.elementToBeClickable(
+                    By.xpath("//a[contains(text(),'Sign In')]")
+            )).click();
 
-            // 2. Login (manual locator, mungkin perlu adjust)
-            driver.findElement(By.xpath("//a[contains(text(),'Sign In')]")).click();
-            Thread.sleep(2000);
 
-            driver.findElement(By.name("email")).sendKeys("qatester2310@gmail.com");
-            driver.findElement(By.name("password")).sendKeys("sandi1023");
+            // 3. Input login
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("email")))
+                    .sendKeys("qatester2310@gmail.com");
 
-            Thread.sleep(10000);
-
-            driver.findElement(By.id("button-login")).click();
-            
-            Thread.sleep(10000);
-
-            // 3. Search product
-            driver.findElement(By.name("filter_name")).sendKeys("Harry Potter");
-            driver.findElement(By.xpath("//button[@type='submit']")).click();
+            driver.findElement(By.name("password"))
+                    .sendKeys("sandi1023");
 
             Thread.sleep(8000);
 
-            // 4. Click first product
-            driver.findElement(By.xpath("(//div[contains(@class,'single-product')]//a)[1]")).click();
+            // 4. Click login
+            driver.findElement(By.id("button-login")).click();
+            
 
-            Thread.sleep(3000);
+            // Tunggu login selesai (misalnya tunggu search bar muncul)
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("filter_name")));
 
-            // 5. Add to cart
-            driver.findElement(By.xpath("//button[contains(text(),'Add to Cart')]")).click();
+            // 5. Search product
+            driver.findElement(By.name("filter_name")).sendKeys("Harry Potter");
+            driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-            Thread.sleep(3000);
+            // 6. Click first product
+            wait.until(ExpectedConditions.elementToBeClickable(
+                    By.xpath("(//div[contains(@class,'single-product')]//a)[1]")
+            )).click();
 
-            // 6. Verify (contoh sederhana: cek cart icon berubah / popup muncul)
-            boolean isAdded = driver.getPageSource().contains("added");
+            // 7. Click Add to Cart
+            WebElement addToCartBtn = wait.until(ExpectedConditions.elementToBeClickable(
+                    By.className("addtocart")
+            ));
+            addToCartBtn.click();
 
-            Assert.assertTrue(isAdded, "Product was not added to cart!");
+            // 8. Verify success notification
+            wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath("//*[contains(text(),'added')]")
+            ));
+
+            // 9. Go to cart page
+            driver.get("https://www.periplus.com/index.php?route=checkout/cart");
+
+            // 10. Verify product exists in cart
+            boolean productExists = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath("//*[contains(text(),'Harry Potter')]")
+            )).isDisplayed();
+
+            Assert.assertTrue(productExists, "Product was not added to cart!");
 
         } catch (Exception e) {
             e.printStackTrace();
+            Assert.fail("Test failed due to exception");
         } finally {
             driver.quit();
         }
